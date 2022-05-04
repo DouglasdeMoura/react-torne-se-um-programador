74cb9db0b2ac0690fb43455264fb4b5b0d5afabe
Douglas Moura <douglas.ademoura@gmail.com>
2022-05-03 22:00:25 -0300

    Criar botão para executar ações na tarefa

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,45 @@
-const tarefas = [
-  {
-    id: 1,
-    nome: 'Estudar React',
-    concluida: false
-  },
-  {
-    id: 2,
-    nome: 'Estudar TypeScript',
-    concluida: false
-  },
-]
+import { useState } from "react"
+
+type AcaoProps = {
+  concluida?: boolean
+} & React.ComponentProps<'button'>
+
+/*
+interface AcaoProps extends React.ComponentProps<'button'> {
+  concluida?: boolean
+}
+*/
+
+function Acao({ concluida, ...props }: AcaoProps) {
+  return <button {...props}>{concluida ? '✅' : '❌'}</button>
+}
 
 function App() {
+  const [tarefas, setTarefas] = useState([
+    {
+      id: 1,
+      nome: 'Estudar React',
+      concluida: false
+    },
+    {
+      id: 2,
+      nome: 'Estudar TypeScript',
+      concluida: false
+    },
+  ])
+
+  const marcarComoConcluida = (id: number) => {
+    setTarefas(tarefas.map(tarefa => {
+      if (tarefa.id === id) {
+        return {
+          ...tarefa,
+          concluida: !tarefa.concluida
+        }
+      }
+      return tarefa
+    }))
+  }
+
   return (
     <div>
       <h1>Tarefas</h1>
@@ -20,7 +48,12 @@ function App() {
         {tarefas.map(tarefa => (
           <li key={tarefa.id}>
             {tarefa.nome}
-            {tarefa.concluida ? ' ✅' : ' ❌'}
+            <Acao
+              concluida={tarefa.concluida}
+              onClick={() => {
+                marcarComoConcluida(tarefa.id)
+              }}
+            />
           </li>
         ))}
       </ul>
