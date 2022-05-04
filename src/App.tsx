003fb2d89ee8363a6073929f8f9e0e113ee5a997
Douglas Moura <douglas.ademoura@gmail.com>
2022-05-03 21:40:01 -0300

    Exibindo um array com usando um map

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,31 @@
+const tarefas = [
+  {
+    id: 1,
+    nome: 'Estudar React',
+    concluida: false
+  },
+  {
+    id: 2,
+    nome: 'Estudar TypeScript',
+    concluida: false
+  },
+]
+
 function App() {
-  return <h1>App</h1>
+  return (
+    <div>
+      <h1>Tarefas</h1>
+
+      <ul>
+        {tarefas.map(tarefa => (
+          <li key={tarefa.id}>
+            {tarefa.nome}
+            {tarefa.concluida ? ' ✅' : ' ❌'}
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
 }
 
 export default App
