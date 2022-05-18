f88a8084890f514f89df5d9c05d222282de72b9c
Douglas Moura <douglas.ademoura@gmail.com>
2022-05-17 22:22:23 -0300

    chore: remove código desnecessário

--- src/components/app/app.services.ts
+++ src/components/app/app.services.ts
@@ -7,14 +7,14 @@ type Tarefa = {
 }
 
 export const getTarefas = (): Promise<Tarefa[]> =>
-  client.get('http://localhost:3000/tarefas').then(response => response.data)
+  client.get('/tarefas').then(response => response.data)
 
 export const addTarefa = (nome: string) => 
-  client.post('http://localhost:3000/tarefas', { nome }).then(response => response.data)
+  client.post('/tarefas', { nome }).then(response => response.data)
 
 
 type UpdateTarefa = Partial<Pick<Tarefa, 'nome' | 'concluida'>> & Pick<Tarefa, 'id'>
 
 export const updateTarefa = ({ id, nome, concluida }: UpdateTarefa) =>
-  client.patch(`http://localhost:3000/tarefas/${id}`, { nome, concluida }).then(response => response.data)
+  client.patch(`/tarefas/${id}`, { nome, concluida }).then(response => response.data)
 
