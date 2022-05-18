7b66e9784894f72096a2d32ec661bfed052db74f
Douglas Moura <douglas.ademoura@gmail.com>
2022-05-17 22:01:06 -0300

    style: adiciona o Pico.css

--- src/main.tsx
+++ src/main.tsx
@@ -13,7 +13,9 @@ const queryClient = new QueryClient()
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <div style={{ margin: '0 auto', maxWidth: '568px' }}>
+        <App />
+      </div>
     </QueryClientProvider>
   </React.StrictMode>
 )
