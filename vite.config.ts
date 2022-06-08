d07417715b2abb71961cf89363cb2cc7774f261c
Douglas Moura <douglas.ademoura@gmail.com>
2022-06-07 22:06:33 -0300

    chore: corrige configuração de cobertura de testes

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,17 @@
-import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vitest/config'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore
   plugins: [react()],
   test: {
     globals: true,
     environment: 'jsdom',
     setupFiles: ['./setupTests.ts'],
-  }
+    coverage: {
+      exclude: ['src/mocks'],
+    },
+  },
 })
