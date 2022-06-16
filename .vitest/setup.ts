80086470316d31201e2fb7974796b8a90297dd45
Douglas Moura <douglas.ademoura@gmail.com>
2022-06-16 17:03:36 -0300

    refactor: move arquivo de configurações do Vitest para seu próprio diretório

--- .vitest/setup.ts
+++ .vitest/setup.ts
@@ -1,5 +1,5 @@
 import '@testing-library/jest-dom/extend-expect'
-import { server } from './src/mocks/server.js'
+import { server } from '../src/mocks/server'
 
 beforeAll(() => server.listen())
 
--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: ['./setupTests.ts'],
+    setupFiles: ['./.vitest/setup.ts'],
     coverage: {
       exclude: ['src/mocks'],
     },
