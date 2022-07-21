b8710e946ae4284768e883f406fe39a2cbeea5a6
Douglas Moura <douglas.ademoura@gmail.com>
2022-07-20 21:23:22 -0300

    feat: caso o token esteja presente, passa-o no cabeçalho da requisição

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,8 +1,16 @@
-import axios from 'axios'
+import axios, { AxiosRequestHeaders } from 'axios'
+
+const token = localStorage.getItem('token') || ''
+
+const headers: AxiosRequestHeaders = {
+  'Content-Type': 'application/json',
+}
+
+if (token) {
+  headers.Authorization = `Bearer ${token}`
+}
 
 export const client = axios.create({
   baseURL: 'http://localhost:3000/',
-  headers: {
-    'Content-Type': 'application/json',
-  },
+  headers,
 })
