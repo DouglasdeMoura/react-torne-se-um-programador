fcc665cec42efa0a0b8a7cb80000f7c20799bd0f
Douglas Moura <douglas.ademoura@gmail.com>
2022-07-27 22:31:06 -0300

    fix: corrige os testes

--- src/pages/login/login.spec.tsx
+++ src/pages/login/login.spec.tsx
@@ -1,16 +1,19 @@
-// import * as reactRouterDom from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 
 import { render, userEvent, screen, waitFor } from '~/utils/test-utils'
 
 import { Login } from '.'
 
-// TODO: descobrir como mockar o useNavigate
-// const navigate = vitest.fn()
-// vi.spyOn(reactRouterDom, 'useNavigate').mockImplementation(() => navigate)
+const App = () => (
+  <Routes>
+    <Route path="/dashboard" element={<div data-testid="dashboard" />} />
+    <Route path="/" element={<Login />} />
+  </Routes>
+)
 
 describe('<Login />', () => {
   it('não deve permitir o envio do formulário sem um usuário e senha', async () => {
-    render(<Login />)
+    render(<App />)
 
     userEvent.tab()
     userEvent.tab()
@@ -26,10 +29,10 @@ describe('<Login />', () => {
   })
 
   it('deve exibir a mensagem de erro quando as credenciais forem inválidas', async () => {
-    render(<Login />)
+    render(<App />)
 
-    userEvent.type(screen.getByLabelText('Usuário'), 'teste')
-    userEvent.type(screen.getByLabelText('Senha'), 'teste')
+    await userEvent.type(screen.getByLabelText('Usuário'), 'teste')
+    await userEvent.type(screen.getByLabelText('Senha'), 'teste')
     userEvent.click(screen.getByText('Entrar'))
 
     await waitFor(() => {
@@ -37,15 +40,15 @@ describe('<Login />', () => {
     })
   })
 
-  // it('deve fazer o login do usuário', async () => {
-  //   render(<Login />)
+  it('deve fazer o login do usuário', async () => {
+    render(<App />)
 
-  //   userEvent.type(screen.getByLabelText('Usuário'), 'admin')
-  //   userEvent.type(screen.getByLabelText('Senha'), 'admin')
-  //   userEvent.click(screen.getByText('Entrar'))
+    await userEvent.type(screen.getByLabelText('Usuário'), 'admin')
+    await userEvent.type(screen.getByLabelText('Senha'), 'admin')
+    userEvent.click(screen.getByText('Entrar'))
 
-  //   await waitFor(() => {
-  //     expect(navigate).toHaveBeenCalledWith('/dashboard')
-  //   })
-  // })
+    await waitFor(() => {
+      expect(screen.getByTestId('dashboard')).toBeInTheDocument()
+    })
+  })
 })
