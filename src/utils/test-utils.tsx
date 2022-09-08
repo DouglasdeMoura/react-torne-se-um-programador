cf383c8a05854ff70cfe053047a5bb12247345df
Douglas Moura <douglas.ademoura@gmail.com>
2022-09-07 21:39:51 -0300

    feat: adiciona mock do useRouter

--- src/utils/test-utils.tsx
+++ src/utils/test-utils.tsx
@@ -6,6 +6,9 @@ import userEvent from '@testing-library/user-event'
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
+// eslint-disable-next-line import-helpers/order-imports
+import type { NextRouter } from 'next/router'
+
 export * from '@testing-library/react'
 
 export const render = (
@@ -18,3 +21,38 @@ export const render = (
     renderOptions,
   ),
 })
+
+const useRouterSpy = vi.spyOn(require('next/router'), 'useRouter')
+
+function createMockRouter(overrides: Partial<NextRouter>) {
+  return {
+    basePath: '',
+    pathname: '/',
+    route: '/',
+    query: {},
+    asPath: '/',
+    back: vi.fn(),
+    beforePopState: vi.fn(),
+    prefetch: vi.fn(),
+    push: vi.fn(),
+    reload: vi.fn(),
+    replace: vi.fn(),
+    events: {
+      on: vi.fn(),
+      off: vi.fn(),
+      emit: vi.fn(),
+    },
+    isFallback: false,
+    isLocaleDomain: false,
+    isReady: true,
+    defaultLocale: 'en',
+    isPreview: false,
+    ...overrides,
+  }
+}
+
+export function mockNextRouter(overrides: Partial<NextRouter> = {}) {
+  const mockRouter = createMockRouter(overrides)
+  useRouterSpy.mockReturnValue(mockRouter)
+  return mockRouter
+}
