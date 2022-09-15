0d00036d4faf987d61ef004b9c92e8b63e42077c
Douglas Moura <douglas.ademoura@gmail.com>
2022-09-14 21:10:24 -0300

    chore: atualiza todas as dependências

--- src/mocks/index.ts
+++ src/mocks/index.ts
@@ -1,13 +1,13 @@
-let environment
-
-if (typeof window === 'undefined') {
-  const { server } = require('./server')
-  server.listen()
-  environment = 'server'
-} else {
-  const { worker } = require('./browser')
-  worker.start()
-  environment = 'browser'
+async function initMocks() {
+  if (typeof window === 'undefined') {
+    const { server } = await import('./server')
+    server.listen()
+  } else {
+    const { worker } = await import('./browser')
+    worker.start()
+  }
 }
 
-export { environment }
+initMocks()
+
+export {}
