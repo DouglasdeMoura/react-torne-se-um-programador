cae3725e496f16883ada741c6eb8274a3ce61e38
Douglas Moura <douglas.ademoura@gmail.com>
2022-10-05 21:41:19 -0300

    chore: remove código não utilizado

--- src/pages/dashboard/index.page.tsx
+++ src/pages/dashboard/index.page.tsx
@@ -1,14 +1,11 @@
-import { IsAuthenticated } from '~/components/is-authenticated'
 import { Menu } from '~/components/menu'
 
 import { Dashboard } from './dashboard'
 
 export default function Page() {
   return (
-    <IsAuthenticated>
-      <Menu>
-        <Dashboard />
-      </Menu>
-    </IsAuthenticated>
+    <Menu>
+      <Dashboard />
+    </Menu>
   )
 }
