d1bad1e272fd76ecceacc1a4ca3fb54fbd75c525
Douglas Moura <douglas.ademoura@gmail.com>
2022-10-19 21:10:23 -0300

    fix: remove componente inexistente

--- src/pages/dashboard/tasks/index.page.tsx
+++ src/pages/dashboard/tasks/index.page.tsx
@@ -1,14 +1,11 @@
-import { IsAuthenticated } from '~/components/is-authenticated'
 import { Menu } from '~/components/menu'
 
 import { Tasks } from './tasks'
 
 export default function Page() {
   return (
-    <IsAuthenticated>
-      <Menu>
-        <Tasks />
-      </Menu>
-    </IsAuthenticated>
+    <Menu>
+      <Tasks />
+    </Menu>
   )
 }
